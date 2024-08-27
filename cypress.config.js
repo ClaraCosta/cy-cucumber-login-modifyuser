9b6e1fff2caf50b2ca3a733962c50c90e725f28d
clara <89088484+ClaraCosta@users.noreply.github.com>
2024-08-27 14:59:40 -0300

    Inserindo o project id

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'eq2oau',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
